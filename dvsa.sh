fe806084acd8be35561b22d2d26b0022660ed734
Tal Melamed <git@appsec.it>
2023-06-14 22:07:31 +0200

    deployment script

--- dvsa.sh
+++ dvsa.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 
+PYHTON_VER=$(python -c 'import sys; print(sys.version_info.major)')
+if [ $PYHTON_VER -ne 3 ]; then
+    PY_CMD=python3
+else
+    PY_CMD=python
+fi
+
+
 case $1 in
 
   package-template)
-    python3 scripts/package.py
+    echo "[-] Will execute SAM with default commands. To modify, update file: scripts/package_template.py"
+    $PY_CMD scripts/package_template.py "${@:2}"
     ;;
 
 
   client-connect)
-    python3 scripts/client_connect.py "${@:2}"
+    $PY_CMD scripts/client_connect.py "${@:2}"
     ;;
 
 
   client-build)
+    BE_STACK=client/src/be-stack.json
+    if [ ! -f "$BE_STACK" ]; then
+        echo "[X] Could not identify $BE_STACK."
+        echo "If you already deployed a backend, please first run: $ ./dvsa.sh client-connect"
+        exit 0
+    fi
     npm run-script client:build
     clear
-    if grep -rnq "<UserPoolClientId>" client/src/be-stack.json
+    if grep -rnq "<UserPoolClientId>" $BE_STACK
     then
         echo "[X] Your client file is not connected to your backend."
-        echo "If you already deployed the client run: ./dvsa.sh connect"
+        echo "If you already deployed the client run: $ ./dvsa.sh client-connect"
         exit 0
     else
         DIST_FOLDER=backend/deployment/dist_s3
@@ -26,6 +41,7 @@ case $1 in
         if [ -d "$DIST_FOLDER" ]; then
             rm -rf $DIST_FOLDER
         fi
+        mkdir $DIST_FOLDER
         cp -r client/dist/* $DIST_FOLDER
         echo "All done! You can now run update-client to update the website."
     fi
@@ -33,12 +49,13 @@ case $1 in
 
   
   client-update)
-    python3 scripts/client_update.py "${@:2}"
+    $PY_CMD scripts/client_update.py "${@:2}"
     ;;
 
 
    *)
-    echo -n "unknown command. options are: package|connect|update. See github.com/owas/dvsa for more information."
+    echo "[X] Unknown command."
+    echo "Options are: package|connect|update. See github.com/owas/dvsa for more information."
     exit 0
     ;;
 
--- scripts/package_template.py
+++ scripts/package_template.py
@@ -1,183 +1,50 @@
-import json
 import os
 import boto3
 from hashlib import md5
-import re
 import argparse
-import shutil
-
-
-
-
-sam_file = "sam_packaged.yml"
-tmp_s3 = "dvsa.serverless.repo"
-sam_final = "sam.yaml"
-cwd = os.getcwd()
-
-aws_regions = ["us-east-1", "us-east-2", "us-west-1", "us-west-2", "ap-northeast-1", "eu-central-1", "eu-west-1", "eu-south-1", "eu-north-1"]
-
 
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-r", "--region", help="Deployment region (default: us-east-1)", default="us-east-1")
-    parser.add_argument("-p", "--profile", help="Deploymen profile (default: default)", default="default")
-    parser.add_argument("-d", "--delete", help="Delete App from Repository", action="store_true")
-    parser.add_argument("-f", "--force", help="Force delete/deployment", action="store_true")
-    parser.add_argument("-v", "--version", help="Update to sepecific version", default="auto")
+    parser.add_argument("-r", "--region", help="Deployment region")
+    parser.add_argument("-p", "--profile", help="Deploymen profile")
     parser.add_argument("-t", "--template", help="The template file (default: template.yml)", default="template.yml")
-    parser.add_argument("-s3", "--s3-repo", help="The S3 bucket to store the repo files (default: dvsa.serverless.repo)", default="dvsa.serverless.repo")
-    parser.add_argument("-c", "--build-client", help="Rebuild client", action="store_true")
-    parser.add_argument("-i", "--debug", help="Debug mode", action="store_true")
+    parser.add_argument("-s3", "--s3-repo", help="The S3 bucket to store the repo files")
+    parser.add_argument("-o", "--output-file", help="the packaged yaml file", default="packaged-template.yml")
+    parser.add_argument("-v", "--verbose", help="Debug mode", action="store_true")
+    parser.add_argument("-f", "--force-upload", help="Force uploading files", action="store_true")
     return parser.parse_args()
-
-
-def pack_dist():
-  # replace parameters with placeholders
-  client_dist = "client/dist"
-  deployment_dist = "backend/deployment/dist_s3"
-  be_stack_file = "client/src/be-stack.json"
-
-  print("[-] replacing placeholders with deployment parameters")
-  with open(be_stack_file) as f:
-    stack = json.loads(f.read())
-
-  with open(f"{client_dist}/bundle.js", "r") as fr:
-    bundle = fr.read()
-
-  for item in stack:
-    k,v = item,stack[item]
-    placeholder = "<{}>".format(k)
-    print(f"replacing: {placeholder} with: {v}... ", end="")
-    bundle = bundle.replace(placeholder, v)
-    if bundle.find(v) > -1 and bundle.find(placeholder) == -1:  
-      print("OK")
-    else:
-      print("FAILED")
-
-  print("[-] copying dist folder to s3_dist")
-  if os.path.exists(deployment_dist):
-    shutil.rmtree(deployment_dist)
-  shutil.copytree(client_dist, deployment_dist)
-  with open(f"{deployment_dist}/bundle.js", "w") as f:
-    f.write(bundle)
-    f.close()
-  return 
-
-
-def update_template_version(infile, v):
-  os.chdir(cwd)
-  with open(infile) as f:
-    template = f.read()
-  old_version = re.search(r"[0-9][.][0-9][.][0-9]{1,3}", template).group()
-  if v == "auto":
-    version = old_version.split(".")
-    sub_inc =  int(version[2]) + 1
-    new_version = f"{version[0]}.{version[1]}.{sub_inc}"
-    print("[-] incrementing {} from: {} to: {}".format(infile, old_version, new_version))
-  else:
-    print("[-] manually setting version to: {}".format(v))
-    new_version = v
-
-  new_template = template.replace(old_version, new_version)
-  with open(infile, "w") as f:
-    f.write(new_template)
-  return new_version
-
 
 
 def main():
   args = get_args()
-  profile = args.profile
-  xregion = args.region
-
-  s3_repo = args.s3_repo
-  build_client = args.build_client
-  template_file = args.template
-
-  if xregion == "all":
-    regions = aws_regions
-  elif ',' in xregion:
-    regions = xregion.split(',')
-  else:
-    regions = [xregion]
-
+  session = boto3.session.Session(profile_name=args.profile, region_name=args.region)
   
-  session = boto3.session.Session(profile_name=profile)
-
-  # --delete
-  if args.delete:
-    print("[-] Deleting app from repo")
-    if not args.force:
-      concent = input("[!] About to empty S3 [{}], would you like to continue [y/N]? > ".format(s3_repo))
-      if concent.lower() != "y":
-        print("cancelling...")
-        return
-
-    s3 = session.resource('s3')
-    bucket = s3.Bucket(s3_repo)
-    bucket.objects.all().delete()
-    return
-
-  
-  # client-side website
-  if not build_client:
-    print("[!] Did not use (-c/--client) to rebuild client-side, skipping.")
-  else:
-    print("[!] Rebuilding client-side!")
-    os.system("npm i")
-    os.system("npm run-script client:build")
-
-  # getting client/dist folder and adding placeholsers
-  pack_dist()
-
-  # build function (SAM)
-  cmd = f"sam build --template {template_file} --parallel"
-  print(f"Running command: {cmd}")
+  print("[-] Validating template... ")
+  cmd = f"sam validate --template {args.template} --lint"
+  print(f"Running command:\n\t. {cmd}")
   os.system(cmd)
 
+  print("[-] Building backend... ")
+  cmd = f"sam build --template {args.template} --parallel"
+  cmd = cmd + " --debug" if args.verbose else cmd
+  print(f"Running command:\n\t. {cmd}")
+  os.system(cmd)
 
   # package template into sam yaml file
-  print("[-] packaging template into sam yaml file")
-  template_hash = -1
-  try:
-    if os.path.exists(sam_file) and not args.force:
-      with open(sam_file, 'rb') as f:
-        template_hash = md5(f.read()).hexdigest()
-    os.remove(sam_file)
-  except OSError:
-      pass
-      
-  template_file = args.template
-
-  cmd = f"sam package --output-template-file {sam_file} --s3-bucket {s3_repo} --profile {profile}"
-  cmd = cmd + " --debug" if args.debug else cmd
-  print(f"Running command: {cmd}")
+  print("[-] Creating a new template with the new code... ")
+  cmd = f"sam package --output-template-file {args.output_file}"
+  cmd = cmd + " --resolve-s3" if args.s3_repo is None else cmd + f" --s3-repo {args.s3_repo}"
+  cmd = cmd + f" --profile {session.profile_name} --region {session.region_name}"
+  cmd = cmd + " --debug" if args.verbose else cmd
+  cmd = cmd + " --force-upload" if args.force_upload else cmd
+  print(f"Running command:\n\t. {cmd}")
   os.system(cmd)
-  new_hash = 0
-  if not args.force and template_hash != -1:
-    with open(sam_file, 'rb') as f:
-      new_hash = md5(f.read()).hexdigest()
 
-
-  if template_hash != new_hash:
-    new_version = update_template_version(template_file, args.version)
-    update_template_version(sam_file, new_version)
-
-    for region in regions:
-      print(f"[-] attempting to publish sam template on region: {region}")
-      if os.path.isfile(sam_file):
-        cmd = "sam publish --template {} --profile {} --region {}".format(sam_file, profile, region)
-        cmd = cmd + " --debug" if args.debug else cmd
-        print(f"Running command: {cmd}")
-        os.system(cmd)
-      
-      else:
-        print("[!] sam package wasn't found.")
-
-  else:
-    print("Nothing was changed...")
-
+  print("\t\t\t\t******************************\n")
+  print("Your packaged template was created successfully!!")
+  print(f"You can now use $ sam deploy or create/update a stack by uploading {args.output_file} through the CloudFormation console/cli")
+  return
 
 if __name__ == "__main__":
   main()
